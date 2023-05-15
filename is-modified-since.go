1c8ed0eafe8787bf3f6dc554c2d1664e55ee38cf
Bogdan Brinza <Bogdan.Brinza@hey.com>
2023-05-14 18:13:47 -0700

    Fixing IsNotModified properly

--- is-modified-since.go
+++ is-modified-since.go
@@ -10,10 +10,12 @@ const (
 	IfModifiedSinceHeader = "If-Modified-Since"
 )
 
-func IsNotModified(ifModifiedSince string, since int64) bool {
-	utcSince := time.Unix(since, 0).UTC()
+func IsNotModified(ifModifiedSince, lastModified string) bool {
+	//utcSince := time.Unix(since, 0).UTC()
 	if ims, err := time.Parse(http.TimeFormat, ifModifiedSince); err == nil {
-		return utcSince.Unix() <= ims.UTC().Unix()
+		if lm, err := time.Parse(http.TimeFormat, lastModified); err == nil {
+			return lm.Unix() <= ims.Unix()
+		}
 	}
 	return false
 }
